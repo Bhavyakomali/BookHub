73d534140df91af7986bc4fff1c28a9e55e2636c
Bhavyakomali <116942886+Bhavyakomali@users.noreply.github.com>
2022-10-29 19:38:17 +0530

    Create index.js

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import {FaGoogle, FaTwitter, FaInstagram, FaYoutube} from 'react-icons/fa'
+
+import './index.css'
+
+const Footer = () => (
+  <div className="footer-container">
+    <div className="footer-icon-container">
+      <FaGoogle className="icon" />
+      <FaTwitter className="icon" />
+      <FaInstagram className="icon" />
+      <FaYoutube className="icon" />
+    </div>
+    <p className="footer-para">Contact Us</p>
+  </div>
+)
+
+export default Footer
